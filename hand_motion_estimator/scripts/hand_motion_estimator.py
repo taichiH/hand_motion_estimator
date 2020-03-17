b0aa00d8c2b5ce60a48ed357a5bca5c251878556
taichiH <higashide@jsk.imi.i.u-tokyo.ac.jp>
2020-03-17 20:27:35 +0900

    scripts/hand_motion_estimator.py

--- hand_motion_estimator/scripts/hand_motion_estimator.py
+++ hand_motion_estimator/scripts/hand_motion_estimator.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 
 import rospy
 import rospkg
-from cv_bridge import CvBridge
+
 from jsk_recognition_msgs.msg import BoundingBoxArray, BoundingBox
 from jsk_rviz_plugins.msg import OverlayText
 from sensor_msgs.msg import Image
@@ -42,36 +42,13 @@ class HandMotionEstimator():
 
         self.interpolate_flow = rospy.get_param('~interpolate_flow', False)
         self.chunk_size = rospy.get_param('~chunk_size', 5)
-        self.bin_size = rospy.get_param('~bin_size', 18)
         self.movement_thresh = rospy.get_param('~movement_thresh', 15)
         self.use_pca = rospy.get_param('~use_pca', False)
-        self.pca_frame = rospy.get_param('~pca_frame', 4)
         self.interpolation_scale = rospy.get_param('~interpolation_scale', 2)
-
-        self.histogram = [0 for i in range(self.bin_size)]
-
         self.flow_chunk = []
-        self.vec_pair = []
 
         self.pca = PCA()
-        self.base_axis = np.array([1,0,0])
 
-        self.motion_lst = ['rot', 'trans', 'other']
-        self.data_path = rospy.get_param(
-            '~model_path',
-            os.path.join(
-                rospkg.RosPack().get_path('hand_motion_estimator'),
-                'trained_data/sample.csv'))
-        self.model = rospy.get_param('~model', 'mlp')
-        self.classifier = None
-        rospy.loginfo('data_path: %s' %(self.data_path))
-        # self.generate_model(self.data_path)
-
-        self.cv_bridge = CvBridge()
-        self.pub_histogram_image = rospy.Publisher(
-            "~output/histogram_image", Image, queue_size=1)
-        self.pub_cross_histogram_image = rospy.Publisher(
-            "~output/cross_histogram_image", Image, queue_size=1)
         self.pub_overlay_text = rospy.Publisher(
             "~output/overlay_text", OverlayText, queue_size=1)
         self.pub_movement = rospy.Publisher(
@@ -79,36 +56,8 @@ class HandMotionEstimator():
         self.pub_motion = rospy.Publisher(
             "~output/motion", Motion, queue_size=1)
 
-        rospy.Service(
-            "~save_histogram", GetHistogram, self.service_callback)
-
         rospy.Subscriber(
             "~input/hand_pose_box", BoundingBoxArray, self.callback)
-
-    def generate_model(self, data_path):
-        if self.model == 'random_forest':
-            self.classifier = RandomForestClassifier(
-                max_depth=2, random_state=0)
-        elif self.model == 'mlp':
-            self.classifier = MLPClassifier(
-                activation='relu', alpha=0.0001, batch_size='auto',
-                solver="adam", random_state=0, max_iter=10000,
-                hidden_layer_sizes=(100,200,100),
-                learning_rate='constant', learning_rate_init=0.001)
-        else:
-            rospy.logwarn('please set classification model')
-
-        test_data = [] #x
-        trained_data = [] #y
-        labels = []
-        with open(self.data_path) as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            for i, row in enumerate(reader):
-                print(row)
-                test_data.append(map(lambda x : float(x), row[1:]))
-                trained_data.append(float(row[0]))
-
-        self.classifier.fit(np.array(test_data), np.array(trained_data))
 
 
     def calc_pca(self, chunk):
@@ -150,68 +99,6 @@ class HandMotionEstimator():
 
             return interpolated_chunk
 
-    def make_angle_buffer(self, chunk, cross_hist):
-        angle_buf = []
-
-        if self.use_pca:
-            self.pca_frame = 4
-        else:
-            self.pca_frame = 0
-        for i in range(2 + self.pca_frame, len(chunk)):
-            if self.use_pca:
-                vec1 = self.calc_pca(chunk[i-1-self.pca_frame:i-1])
-                vec2 = self.calc_pca(chunk[i-self.pca_frame:i])
-            else:
-                vec1 = chunk[i-1] - chunk[i-2]
-                vec2 = chunk[i]   - chunk[i-1]
-
-            cross = None
-            if cross_hist:
-                cross = np.cross(vec1, vec2)
-            else:
-                cross = vec1
-
-            dot = np.dot(self.base_axis, cross)
-            angle = np.arccos(
-                dot / (np.linalg.norm(cross) * np.linalg.norm(self.base_axis)))
-            angle = np.rad2deg(angle)
-
-            if np.isnan(angle):
-                continue
-            angle_buf.append(angle)
-
-        return angle_buf
-
-    def make_histogram(self, angle_buf):
-        hist = [0 for i in range(self.bin_size)]
-        for angle in angle_buf:
-            idx = int(math.floor(angle / 10.))
-            hist[idx] += 1
-        histogram = np.array(hist) / float(np.array(hist).max())
-        return histogram
-
-    def classify_motion(self, target_data):
-        if self.classifier is None:
-            return None
-
-        motion_class = self.classifier.predict(np.array([target_data]))
-        motion_class = int(motion_class[0])
-        return self.motion_lst[motion_class]
-
-    def service_callback(self, req):
-        res = GetHistogramResponse()
-
-        save_data =  [req.label]  + list(self.histogram)
-        print('save_data', save_data)
-
-        with open(self.data_path, 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(save_data)
-
-        res.success = True
-        res.histogram = self.histogram
-        return res
-
     def callback(self, boxes_msg):
         text_msg = OverlayText()
         text_msg.text_size = 15
@@ -229,10 +116,8 @@ class HandMotionEstimator():
 
         interpolated_chunk = self.make_flow_chunk(pos)
         if interpolated_chunk == self.state.not_buffered:
-            # rospy.loginfo('flow_chunk: %s' %(self.state.not_buffered.name))
             return
         elif interpolated_chunk == self.state.error:
-            # rospy.logwarn('failed calc spline')
             return
 
         pca_vec = self.calc_pca(interpolated_chunk)
@@ -263,10 +148,6 @@ class HandMotionEstimator():
             self.flow_chunk = []
         else:
             move_state = self.move_state.move
-
-        # print('movement: ', movement)
-        # print('most moved axis is %s' %(most_move_axis))
-        # print('motion: ', motion)
 
         max_count = 0
         self.motions_count[motion] += 1
